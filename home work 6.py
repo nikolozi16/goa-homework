0ec4c9e4ac4788ad766203a79ef36dbecba8b9db
nikolozi16 <n.cindeliani13@gmail.com>
2024-06-02 12:15:50 +0400

    second commit

---
+++ home work 6.py
@@ -0,0 +1,21 @@
+user_firstname = input("please enter firstname: ")
+user_lastname = input("please enter lastname: ")
+user_age = input("please enter age: ")
+user_email = input("please enter email: ")
+user_addres = input("please enter addres: ")
+user_number = input("please enter number: ")
+
+num1 = user_mom_age = int(input("please enter mom age: "))
+num2 = user_dad_age = int(input("please enter dad age: "))
+num3 = user_sister_age = int(input("please enter sister age: "))
+num4 = user_brother_age = int(input("please enter brother age: "))
+print(str(num1 + 20))
+print(str(num2 + 20))
+print(str(num3 + 20))
+print(str(num4 + 20))
+
+
+
+
+print("your inputed information is:",user_firstname,user_lastname,user_age,user_email,user_addres,user_number,user_mom_age,user_dad_age,user_sister_age,user_brother_age,)
+
